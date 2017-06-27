87ca1a9e741113eb0e58680fd92105e6ab2b2de8
Simon Bergström <simon@menuan.se>
2017-06-27 07:53:44 +0200

    Weird merge thing.

--- lib/slack/supervisor.ex
+++ lib/slack/supervisor.ex
@@ -1,16 +1,32 @@
 defmodule Kemisten.Slack.Supervisor do
   use Supervisor
-  alias Slack.Bot
-  @name Kemisten.Slack.Supervisor
+
+  def start_bot(token, opts \\ []) do
+    options = Keyword.merge([name: __MODULE__], opts)
+    name = String.to_atom("bot_" <> token)
+
+    case Process.whereis(name) do
+      pid when is_pid(pid) -> pid
+      nil ->
+        bot_params = [RtmBot, [], token, %{name: name}]
+        case Supervisor.start_child(options[:name], bot_params) do
+          {:ok, pid} -> pid
+          {:error, {:EXIT, {{:badkey, _, err}, _}}} ->
+            IO.puts "Failed to start child:"
+            IO.inspect err
+            err # complex error structure from the Slack.Bot failures
+        end
+    end
+  end
   
-  def start_link do
-    Supervisor.start_link(__MODULE__, :ok, name: @name)
+  def start_link(opts \\ []) do
+    options = Keyword.merge([ name: __MODULE__ ], opts)
+    Supervisor.start_link(__MODULE__, [], name: options[:name])
   end
 
-  def init(:ok) do
+  def init([]) do
     children = [
-      # botArgs = [SlackKemisten, [], "xoxb-188440640803-4BY20SzzVG0JBNjxKXIgZHdI"]
-      worker(Slack.Bot, [], restart: :permanent)
+      worker(Slack.Bot, [], restart: :transient)
     ]
     supervise(children, strategy: :simple_one_for_one);
   end
