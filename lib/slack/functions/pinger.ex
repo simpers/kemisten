89f6e53bf53befc7fd515837be5a2ecc5f7b589b
Simon Bergström <simon@menuan.se>
2018-07-25 13:48:49 +0200

    Fixed the errors.

--- lib/slack/functions/pinger.ex
+++ lib/slack/functions/pinger.ex
@@ -1,4 +1,6 @@
 defmodule Kemisten.Pinger do
+  require Logger
+  
   alias Slack.Sends
 
   alias Kemisten.Utils
@@ -6,30 +8,30 @@ defmodule Kemisten.Pinger do
   def ping_response(channel, slack), do: Sends.send_message("pong", channel, slack)
 
   def setup_pinger(channel, state, slack) do
-    { :ok, timer_ref } = :timer.apply_interval(5000, __MODULE__, :ping_channel, [user_id, slack])
-    { :ok, Kernel.put_in(state, [:pinging, user_id], timer_ref) }
+    { :ok, timer_ref } = :timer.apply_interval(5000, __MODULE__, :ping_channel, [channel, slack])
+    { :ok, Kernel.put_in(state, [:pinging, channel], timer_ref) }
   end
 
   def stop_pinger(channel, state) do
-    case Kernel.pop_in(state[:pinging][user_id]) do
+    case Kernel.pop_in(state[:pinging][channel]) do
       { nil, state } ->
-        Logger.debug "User #{user_id} not found in active pings."
+        Logger.debug "User #{channel} not found in active pings."
         { :ok, state }
       { timer_ref, new_state } ->
-        Logger.info "Stopped pinging user #{user_id}"
+        Logger.info "Stopped pinging user #{channel}"
         { :ok, :cancel } = :timer.cancel(timer_ref)
         { :ok, new_state }
     end
   end
 
-  def pong_response(user_id, state, slack) do
-    case Kernel.pop_in(state[:pinging][user_id]) do
+  def pong_response(channel, state, slack) do
+    case Kernel.pop_in(state[:pinging][channel]) do
       { nil, state } ->
-        Logger.debug "Was not pinging user #{user_id}."
+        Logger.debug "Was not pinging user #{channel}."
         { :ok, state }
       { timer_ref, new_state } ->
         { :ok, :cancel } = :timer.cancel(timer_ref)
-        Sends.send_message(":)", user_id, slack)
+        Sends.send_message(":)", channel, slack)
         { :ok, new_state }
     end
   end
--- lib/slack/functions/utils.ex
+++ lib/slack/functions/utils.ex
@@ -2,6 +2,6 @@ defmodule Kemisten.Utils do
 
   def extract_user_id(binary) when is_binary(binary) do
     regex = ~r/<@(?<id>[A-Z0-9]{9})>/u
-    Regex.named_captures(regex, user)["id"]
+    Regex.named_captures(regex, binary)["id"]
   end
 end
